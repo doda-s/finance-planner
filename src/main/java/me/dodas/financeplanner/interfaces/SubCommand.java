23206d1d8341281c109b3afa250c71be20fea12d
ssumik <joao.sumi@gmail.com>
2024-11-19 19:50:30 -0300

    refactor:deleted the aliases from the subcommand interface

--- src/main/java/me/dodas/financeplanner/interfaces/SubCommand.java
+++ src/main/java/me/dodas/financeplanner/interfaces/SubCommand.java
@@ -5,6 +5,5 @@ import java.util.List;
 public interface SubCommand {
         public String getName();
         public String getDescription();
-        public String[] getAliases();
         public void executeCommand(List<String> args);       
 }
