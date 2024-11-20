b2e5e0826ce35a3a2d9b2a63fc71fa7a1144a5f2
Dodas_ <ph005009004@gmail.com>
2024-11-20 18:33:15 -0300

    feat:add user input

--- src/main/java/me/dodas/financeplanner/Main.java
+++ src/main/java/me/dodas/financeplanner/Main.java
@@ -1,20 +1,35 @@
 package me.dodas.financeplanner;
 
-import java.time.LocalDate;
+import java.util.Scanner;
 
+import me.dodas.financeplanner.commands.HelpCommand;
+import me.dodas.financeplanner.entities.CommandManager;
 import me.dodas.financeplanner.entities.ConfigManager;
 import me.dodas.financeplanner.entities.DirectoryManager;
-import me.dodas.financeplanner.entities.SpreadsheetManager;
 
 public class Main {
-    static ConfigManager configManager = ConfigManager.getInstance();
-    static DirectoryManager directoryManager = DirectoryManager.getInstance();
+    private static ConfigManager configManager = ConfigManager.getInstance();
+    private static DirectoryManager directoryManager = DirectoryManager.getInstance();
+    private static CommandManager commandManager = CommandManager.getInstance();
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static Boolean run = true;
 
     public static void main(String[] args) {
 
         configManager.loadConfig();
         checkDirectories();
+        loadCommands();
+
+        do {
+            commandManager.commandListener(scanner.nextLine());
+        } while (run);
         
+        scanner.close();
+    }
+
+    public void stopRunning() {
+        run = false;
     }
 
     // Check if directories exists
@@ -26,5 +41,9 @@ public class Main {
         }
 
         System.out.println("Directories checked successfully.");
+    }
+
+    private static void loadCommands() {
+        commandManager.commandRegister(new HelpCommand());
     }
 }
