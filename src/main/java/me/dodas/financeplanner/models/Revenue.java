b981ecb55df1ad2b885a76df87e0b0bb20d07137
Dodas_ <ph005009004@gmail.com>
2024-12-01 19:43:12 -0300

    refactor:add registry abstract class

--- src/main/java/me/dodas/financeplanner/models/Revenue.java
+++ src/main/java/me/dodas/financeplanner/models/Revenue.java
@@ -1,46 +1,51 @@
 package me.dodas.financeplanner.models;
 
-public class Revenue{
+public class Revenue extends Registry {
 
+    private String id;
     private String name;
     private String description;
     private double value;
     private String creationDate;
 
-        
-        public String getName(){
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
+    public Revenue (String id) {
+        this.id = id;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public String getName(){
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getDescription(){
+        return description;
+    }
 
-        public String getDescription(){
-            return description;
-        }
-        public void setDescription(String description) {
-            this.description = description;
-        }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public double getValue(){
+        return value;
+    }
 
+    public void setValue(double value) {
+        this.value = value;
+    }
 
-        public double getValue(){
-            return value;
-        }
-        public void setValue(double value) {
-            this.value = value;
-        }
+    public String getCreationDate(){
+        return creationDate;
+    }
 
-
-
-        public String getCreationDate(){
-            return creationDate;
-        }
-        public void setCreationDate(String creationDate) {
-            this.creationDate = creationDate;
-        }
-
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
 
 }
